ba1f7f62733fef7f81dc7138ffdc14dc0b789585
Cao Jie <jacky_cao@dnt.com.cn>
2014-07-21 18:14:05 +0800

    add service catalog service (example)

---
+++ backend/services/servicecatalog_service/servicecatalog_api/src/main/java/dnt/itsnow/services/model/ServiceCatalog.java
@@ -0,0 +1,31 @@
+package dnt.itsnow.services.model;
+
+import dnt.itsnow.platform.model.Record;
+
+/**
+ * The ServiceCatalog Object
+ */
+public class ServiceCatalog extends Record {
+
+    private String    scName;
+    private String    scDesc;
+
+    public String getScName() {
+        return scName;
+    }
+
+    public void setScName(String scName) {
+        this.scName = scName;
+    }
+
+    public String getScDesc() {
+        return scDesc;
+    }
+
+    public void setScDesc(String scDesc) {
+        this.scDesc = scDesc;
+    }
+
+
+
+}
