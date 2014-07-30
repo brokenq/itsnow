656b7d5e5036194fdfb15d9bdc90f8aba07d5dd6
stone <stone5751@126.com>
2014-07-30 14:22:23 +0800

    commit contract

---
+++ frontend/src/app/contract/contract-list.js
@@ -0,0 +1,90 @@
+/**
+ * Created by User on 2014/7/21.
+ */
+var contractApp = angular.module('contractApp', ['ngGrid','ngResource']);
+
+var ContractListCtrl = ['$scope', '$http', 'ContractService', function ($scope, $http, ContractService) {
+    $scope.filterOptions = {
+        filterText: "",
+        useExternalFilter: true
+    };
+    $scope.totalServerItems = 0;
+    $scope.pagingOptions = {
+        pageSizes: [10],
+        pageSize: 10,
+        currentPage: 1
+    };
+    $scope.setPagingData = function (data, page, pageSize) {
+        var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+        $scope.datas = pagedData;
+        $scope.totalServerItems = data.length;
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    };
+    $scope.getPagedDataAsync = function (pageSize, page, searchText) {
+        setTimeout(function () {
+            var data = [
+                {id: '001', name: '合同1', resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '002', name: '合同2',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '003', name: '合同3',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '004', name: '合同4',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '005', name: '合同5',  resolutionTime: '201407211258', responseTime: '201407211258'}
+            ];
+            if (searchText) {
+//                var ft = searchText.toLowerCase();
+//                ContractService.list(function(largeLoad){
+//                    data = largeLoad.filter(function (item) {
+//                        return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+//                    });
+//                    $scope.setPagingData(data, page, pageSize);
+//                });
+                    $scope.setPagingData(data, page, pageSize);
+            } else {
+//                ContractService.list(function(largeLoad){
+//                    $scope.setPagingData(largeLoad, page, pageSize);
+//                });
+                    $scope.setPagingData(data, page, pageSize);
+            }
+        }, 100);
+    };
+
+    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+
+    $scope.$watch('pagingOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
+    $scope.$watch('filterOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
+
+    $scope.gridContractList = {
+        data: 'datas',
+        enablePaging: true,
+        showFooter: true,
+        totalServerItems: 'totalServerItems',
+        pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions,
+        multiSelect: false,
+        columnDefs: [
+            {field: 'id', displayName: 'ID'},
+            {field: 'name', displayName: '合同名称'}
+        ]
+    };
+}];
+
+contractApp.controller('ContractListCtrl', ContractListCtrl);
+
+// 封装$http
+contractApp.factory('ContractService', ['$resource', function ($resource) {
+    return $resource('/api/contract', null, {
+        list: {
+            method: 'GET',
+            isArray: true
+        }
+    });
+}]);
---
+++ frontend/src/app/contract/contract-list.spec.js
@@ -0,0 +1,38 @@
+/**
+ * Created by User on 2014/7/21.
+ */
+describe('user controllers', function () {
+
+    var scope, ctrl, $httpBackend;
+
+    beforeEach(module('contractApp'));
+
+    beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('/api/contract').
+            respond([
+                {id: '001', name: '协议1', resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '002', name: '协议2',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '003', name: '协议3',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '004', name: '协议4',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '005', name: '协议5',  resolutionTime: '201407211258', responseTime: '201407211258'}
+            ]);
+
+        scope = $rootScope.$new();
+        ctrl = $controller(ContractListCtrl, {$scope: scope});
+    }));
+
+    it('should create "user" model with 5 users fetched from xhr', function () {
+        setTimeout(function () {
+            $httpBackend.flush();
+            expect(scope.datas).toEqual([
+                {id: '001', name: '协议1', resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '002', name: '协议2',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '003', name: '协议3',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '004', name: '协议4',  resolutionTime: '201407211258', responseTime: '201407211258'},
+                {id: '005', name: '协议5',  resolutionTime: '201407211258', responseTime: '201407211258'}
+            ]);
+        }, 1000);
+    });
+
+});
