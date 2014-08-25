e2e58f5f698d7006c3605036e3c6c562b2cb3272
Kadvin <kadvin@gmail.com>
2014-08-25 14:04:59 +0800

    init the ms(c|u|p)-gui-service

---
+++ backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20140825140057_insert_msc_menu_items.sql
@@ -0,0 +1,11 @@
+-- // insert_msc_menu_items
+-- Migration SQL that makes the change goes here.
+
+-- INSERT INTO menu_items() VALUES
+-- (),
+-- ();
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+TRUNCATE TABLE menu_items;
+
