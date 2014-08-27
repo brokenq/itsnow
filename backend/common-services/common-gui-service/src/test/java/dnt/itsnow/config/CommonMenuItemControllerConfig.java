882e9794d56cf9c822d34b813cd3eaa621467c8a
unknown <stone5751@126.com>
2014-08-27 14:24:45 +0800

    fix bug

--- backend/common-services/common-gui-service/src/test/java/dnt/itsnow/config/CommonMenuItemControllerConfig.java
+++ backend/common-services/common-gui-service/src/test/java/dnt/itsnow/config/CommonMenuItemControllerConfig.java
@@ -4,6 +4,7 @@ import dnt.itsnow.service.CommonMenuItemService;
 import dnt.itsnow.test.config.ApplicationControllerConfig;
 import dnt.itsnow.web.controller.CommonMenuItemController;
 import org.easymock.EasyMock;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,11 @@ public class CommonMenuItemControllerConfig extends ApplicationControllerConfig 
     @Bean
     public CommonMenuItemService commonMenuItemService(){
         return EasyMock.createMock(CommonMenuItemService.class);
+    }
+
+    @Bean
+    public CommonMenuItemController commonMenuItemController(){
+        return new CommonMenuItemController();
     }
 
 }
--- backend/common-services/common-gui-service/src/test/java/dnt/itsnow/web/controller/CommonMenuItemControllerTest.java
+++ backend/common-services/common-gui-service/src/test/java/dnt/itsnow/web/controller/CommonMenuItemControllerTest.java
@@ -1,50 +1,67 @@
 package dnt.itsnow.web.controller;
 
 import dnt.itsnow.config.CommonMenuItemControllerConfig;
+import dnt.itsnow.model.MenuItem;
 import dnt.itsnow.service.CommonMenuItemService;
 import dnt.itsnow.test.controller.ApplicationControllerTest;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.easymock.EasyMock.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by Sin on 2014/8/26.
  */
 @ContextConfiguration(classes = CommonMenuItemControllerConfig.class)
-@ActiveProfiles("test")
-@RunWith(SpringJUnit4ClassRunner.class)
 public class CommonMenuItemControllerTest extends ApplicationControllerTest {
 
     @Autowired
     CommonMenuItemService commonMenuItemService;
 
+    List<MenuItem> menuItemList;
+
+    @Before
+    public void setup() {
+
+        menuItemList = new ArrayList<MenuItem>();
+        MenuItem menuItem = new MenuItem();
+        menuItem.setId(1L);
+        menuItem.setParentId(null);
+        menuItem.setName("用户");
+        menuItem.setUrl("www.google.com");
+        menuItemList.add(menuItem);
+
+        reset(commonMenuItemService);
+    }
+
     @Test
     public void testShow() throws Exception {
-        // Service Mock 记录阶段
-        expect(commonMenuItemService.findAll());
+
+        expect(commonMenuItemService.findAll())
+                .andReturn(menuItemList);
+
+        replay(commonMenuItemService);
 
         // 准备 Mock Request
         MockHttpServletRequestBuilder request = get("/api/menu");
-        decorate(request);
-
-        // Mock 准备播放
-        replay(commonMenuItemService);
+        request = decorate(request);
 
         // 执行
         ResultActions result = this.browser.perform(request);
 
         // 对业务结果的验证
         decorate(result).andExpect(status().isOk());
+
     }
 
     // 每次测试结束之后再验证
