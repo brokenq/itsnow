08ada07bb9a7b0fa529b51dc653c294a3fdbab1e
Kadvin <kadvin@gmail.com>
2014-09-12 15:47:47 +0800

    menu items

--- frontend/src/index/menu/menu.js
+++ frontend/src/index/menu/menu.js
@@ -1,23 +1,8 @@
 /**
  * Created by User on 2014/7/23.
  */
-var appMenu = angular.module('Index.Menu', ['ui.tree']);
-
-appMenu.factory('MenuService', ['$resource', function ($resource) {
-    return $resource('/api/menu_items');
-}]);
-
-var menuCtrl = appMenu.controller('MenuCtrl', ['$scope', 'MenuService', function ($scope, menuService) {
-
+angular.module('Index.Menu', []).
+  controller('MenuCtrl', ['$scope', '$resource', function ($scope, $resource) {
+    var menuService = $resource('/api/menu_items');
     $scope.topMenuItems = menuService.query();
-
-    $scope.toggle = function(scope) {
-        if(scope.collapse){
-            scope.expand();
-        }else{
-            scope.collapse();
-        }
-    };
-
-}]);
-
+  }]);
