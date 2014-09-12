6443e3c800aee83cca111ff2578ae507c8c65744
Cao Jie <jacky_cao@dnt.com.cn>
2014-09-12 10:36:10 +0800

    fix migrate error for common-services and mutable-user-service

--- backend/common-services/common-model-service/src/main/resources/META-INF/migrate/20140722135425_create_group_authorities.sql
+++ backend/common-services/common-model-service/src/main/resources/META-INF/migrate/20140722135425_create_group_authorities.sql
@@ -5,8 +5,7 @@ CREATE TABLE group_authorities (
     id         INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     group_id  INT(10) UNSIGNED  NOT NULL,
     authority VARCHAR(255)     NOT NULL,
-    FOREIGN KEY (group_id) REFERENCES groups (id),
-    FOREIGN KEY (authority) REFERENCES roles (name)
+    FOREIGN KEY (group_id) REFERENCES groups (id)
 );
 
 -- //@UNDO
