a1060a03b5dc97e0835ba73c1d123971be8854cb
Kadvin <kadvin@gmail.com>
2014-09-12 16:08:33 +0800

    change menu item migrate

--- backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20140825140057_insert_msc_menu_items.sql
+++ backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20140825140057_insert_msc_menu_items.sql
@@ -1,23 +1,23 @@
 -- // insert_msc_menu_items
 -- Migration SQL that makes the change goes here.
 
-INSERT INTO menu_items(id, parent_id, name, state, position) VALUES
-(1, null, '帐户管理', 'index.accounts',       0),
-(2, null, '合同管理',  'index.contracts',     1),
-(3, null, '服务管理', 'index.services',       2),
-(4, null, '系统管理', 'index.system',         3),
-(11, 1, '企业帐户', 'index.accounts.msu',     0), 
-(12, 1, '服务商帐户', 'index.accounts.msp',   1), 
-(13, 1, '服务进程', 'index.accounts.service', 2),
+INSERT INTO menu_items(id, parent_id, name, state, position, css) VALUES
+(1, null, '帐户管理',   'accounts',          0, 'icon-globe'),
+(2, null, '合同管理',   'contracts',         1, 'icon-th-list'),
+(3, null, '服务管理',   'services',          2, 'icon-tag'),
+(4, null, '系统管理',   'system',            3, 'icon-tag'),
+(11,   1, '企业帐户',   'accounts.msu',      0, 'icon-tag'), 
+(12,   1, '服务商帐户', 'accounts.msp',      1, 'icon-tag'), 
+(13,   1, '服务进程',   'accounts.service',  2, 'icon-play'),
 
-(21, 2, '合同管理', 'index.contracts.contract', 3),
+(21,   2, '合同管理',   'contracts.contract',3, 'icon-tag'),
 
-(31, 3, '服务目录', 'index.services.catalog', 0),
-(32, 3, 'SLA管理', 'index.services.sla',      1),
+(31,   3, '服务目录',   'services.catalog',  0, 'icon-tag'),
+(32,   3, 'SLA管理',    'services.sla',      1, 'icon-eye-open'),
 
-(41, 4, '角色管理', 'index.system.role',      0),
-(42, 4, '用户管理', 'index.system.user',      1),
-(43, 4, '权限管理', 'index.system.privilege', 2);
+(41,   4, '角色管理',   'system.role',       0, 'icon-group'),
+(42,   4, '用户管理',   'system.user',       1, 'icon-user'),
+(43,   4, '权限管理',   'system.privilege',  2, 'icon-authenticate');
 -- //@UNDO
 -- SQL to undo the change goes here.
 
