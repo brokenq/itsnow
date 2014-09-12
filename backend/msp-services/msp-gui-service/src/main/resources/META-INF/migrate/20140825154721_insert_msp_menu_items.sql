3eac009e3358e3febc61306b212a249e9e746c69
Cao Jie <jacky_cao@dnt.com.cn>
2014-09-12 18:01:17 +0800

    Add contract list in frontend and modify menu migrate sql of msu/p

--- backend/msp-services/msp-gui-service/src/main/resources/META-INF/migrate/20140825154721_insert_msp_menu_items.sql
+++ backend/msp-services/msp-gui-service/src/main/resources/META-INF/migrate/20140825154721_insert_msp_menu_items.sql
@@ -1,32 +1,31 @@
 -- // insert_menu_items.sql
 -- Migration SQL that makes the change goes here.
 
-INSERT INTO menu_items(id, parent_id, name, state, url, template_url, position) VALUES
-(1, null, '系统配置', 'index.system',            0),
-(3, null, '服务管理', 'index.services',          1),
-(2, null, '合同管理',  'index.contracts',        2),
-(4, null, '故障管理',  'index.incidents',        3),
+INSERT INTO menu_items(id, parent_id, name, state, position, css) VALUES
+(1, null, '系统配置', 'system',0, 'icon-tag'),
+(3, null, '服务管理', 'services',1, 'icon-tag'),
+(2, null, '合同管理',  'contracts',2, 'icon-th-list'),
+(4, null, '故障管理',  'incidents',3, 'icon-warning-sign'),
 
-(11, 1, '角色管理', 'index.system.role',         0),
-(12, 1, '用户管理', 'index.system.user',         1),
-(13, 1, '权限管理', 'index.system.privilege',    2),
-(14, 1, '部门管理', 'index.system.department',   3),
-(15, 1, '工作时间', 'index.system.worktime',     4),
-(16, 1, '地点管理', 'index.system.site',         5),
-(17, 1, '员工管理', 'index.system.staff',        6),
-(18, 1, '组管理',   'index.system.group',        7),
+(11, 1, '角色管理', 'system.role', 0, 'icon-group'),
+(12, 1, '用户管理', 'system.user', 1, 'icon-user'),
+(13, 1, '权限管理', 'system.privilege', 2, 'icon-authenticate'),
+(14, 1, '部门管理', 'system.department',3, 'icon-th-list'),
+(15, 1, '工作时间', 'system.worktime',  4, 'icon-time'),
+(16, 1, '地点管理', 'system.site',      5, 'icon-home'),
+(17, 1, '员工管理', 'system.staff',     6, 'icon-user'),
+(18, 1, '组管理',   'system.group',     7, 'icon-th-large'),
 
-(21, 2, '合同管理', 'index.contracts.contract', 0),
+(21, 2, '合同管理', 'contracts.contract',0, 'icon-tag'),
 
-(31, 3, '服务目录', 'index.services.catalog',   0),
-(32, 3, 'SLA管理', 'index.services.sla',        1),
-(33, 3, '字典管理', 'index.services.dict',      1),
-(34, 3, '流程管理', 'index.services.workflow',  1),
+(31, 3, '服务目录', 'services.catalog', 0, 'icon-th'),
+(32, 3, 'SLA管理', 'services.sla',      1, 'icon-eye-open'),
+(33, 3, '字典管理', 'services.dict',    1, 'icon-book'),
+(34, 3, '流程管理', 'services.workflow',1, 'icon-task'),
 
-(41, 4, '新建故障',   'index.incidents.create', 1),
-(42, 4, '我的故障单', 'index.incidents.my',     2),
-(43, 4, '已关闭故障', 'index.incidents.closed', 3);
-
+(41, 4, '新建故障',   'incidents.create', 1, 'icon-fire'),
+(42, 4, '我的故障单', 'incidents.opened',     2, 'icon-eye-open'),
+(43, 4, '已关闭故障', 'incidents.closed', 3, 'icon-eye-close');
 -- //@UNDO
 -- SQL to undo the change goes here.
 
