f722bf60dc15af9f72a0ea5eeaedced08aeecc4a
Cao Jie <jacky_cao@dnt.com.cn>
2014-09-26 09:54:16 +0800

    Fix contract migrate sql error

--- backend/msc-services/mutable-contract-service/src/main/resources/META-INF/migrate/20140728144841_insert_contracts.sql
+++ backend/msc-services/mutable-contract-service/src/main/resources/META-INF/migrate/20140728144841_insert_contracts.sql
@@ -7,9 +7,9 @@ SET @msp_001_id = (SELECT id from accounts where sn = 'msp_001');
 SET @msp_002_id = (SELECT id from accounts where sn = 'msp_002');
 
 INSERT INTO contracts(msu_account_id, msp_account_id, sn,msu_status,msp_status) VALUES
-(@msu_001_id, @msp_002_id, 'SWG-201403080003','Draft','Purpose'),
-(@msu_001_id, @msp_001_id, 'SWG-201407010010','Approved','Purpose'),
-(@msu_001_id, @msp_001_id, 'SWG-201408050002','Rejected','Purpose');
+(@msu_001_id, @msp_002_id, 'SWG-201403080003','Draft','Purposed'),
+(@msu_001_id, @msp_001_id, 'SWG-201407010010','Approved','Purposed'),
+(@msu_001_id, @msp_001_id, 'SWG-201408050002','Rejected','Purposed');
 
 -- //@UNDO
 -- SQL to undo the change goes here.
