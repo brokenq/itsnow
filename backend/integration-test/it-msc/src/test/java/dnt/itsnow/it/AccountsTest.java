8d55d62b0c02fc2dc8d4593a7c0ab584110cb397
brokenq <brokenq@163.com>
2014-10-24 17:19:14 +0800

    ignore test: AccountsTest and ProfileTest

--- backend/integration-test/it-msc/src/test/java/dnt/itsnow/it/AccountsTest.java
+++ backend/integration-test/it-msc/src/test/java/dnt/itsnow/it/AccountsTest.java
@@ -7,6 +7,7 @@ import dnt.itsnow.model.ClientAccount;
 import dnt.itsnow.model.ClientAccountRegistration;
 import dnt.itsnow.model.ClientUser;
 import junit.framework.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 /**
  * 测试MSC帐户管理功能
  */
+@Ignore
 public class AccountsTest extends AbstractTest {
 
 
