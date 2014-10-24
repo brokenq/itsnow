fa5217bafcbfb38214bb17cc0b83d0439d96430f
Stone Xin <stone5751@126.com>
2014-10-24 15:06:36 +0800

    fixed siteManagerTest bug

--- backend/general-services/general-model-service/src/test/java/itsnow/dnt/config/SiteRepositoryConfig.java
+++ backend/general-services/general-model-service/src/test/java/itsnow/dnt/config/SiteRepositoryConfig.java
@@ -17,6 +17,9 @@ public class SiteRepositoryConfig extends RepositoryConfigWithH2 {
 
     protected String[] sqlScripts() {
         return new String[]{
+                "classpath:META-INF/setup/create_accounts.sql@up",
+                "classpath:META-INF/setup/create_users.sql@up",
+                "classpath:META-INF/setup/create_staffs.sql@up",
                 "classpath:META-INF/migrate/20140720144647_create_dictionaries.sql@up",
                 "classpath:META-INF/migrate/20140721113250_create_work_times.sql@up",
                 "classpath:META-INF/migrate/20140722130944_create_sites.sql@up",
@@ -25,6 +28,9 @@ public class SiteRepositoryConfig extends RepositoryConfigWithH2 {
 
                 "classpath:META-INF/setup/initialization_site.sql",
 
+                "classpath:META-INF/setup/insert_accounts.sql@up",
+                "classpath:META-INF/setup/insert_users.sql@up",
+                "classpath:META-INF/setup/insert_staffs.sql@up",
                 "classpath:META-INF/migrate/20140904124504_insert_dictionaries.sql@up",
                 "classpath:META-INF/setup/insert_work_times.sql@up",
                 "classpath:META-INF/migrate/20140909150904_insert_sites.sql@up",
--- backend/general-services/general-model-service/src/test/resources/META-INF/setup/initialization_site.sql
+++ backend/general-services/general-model-service/src/test/resources/META-INF/setup/initialization_site.sql
@@ -3,3 +3,6 @@ DELETE FROM sites;
 DELETE FROM departments;
 DELETE FROM work_times;
 DELETE FROM dictionaries;
+DELETE FROM staffs;
+DELETE FROM itsnow_msc.users;
+DELETE FROM itsnow_msc.accounts;
