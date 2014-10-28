1aafab8dae236293a5f6872877c42aa057ce797f
amrocky <amrocky@163.com>
2014-10-28 16:57:08 +0800

    repair dict select

--- frontend/src/msp/index/service/dict/detail.js
+++ frontend/src/msp/index/service/dict/detail.js
@@ -16,15 +16,21 @@ angular.module('Service.Dictnew', ['ngTable', 'ngResource'])
     .controller('DictNewCtrl', ['$http','$rootScope','$scope', '$location', '$timeout', '$state','$stateParams', 'DictService',
         function ($http,$rootScope,$scope, $location, $timeout, $state,$stateParams, DictService) {
         var sn=$stateParams.sn;
-        $scope.datas=[
-            {name: '有效',id:'1',state:'1'},
-            {name: '无效',id:'2',state:'2'}
-         ];
+            $scope.datas=[{name:"无效",value:0},{name:"有效",value:1}];
+
         if (sn !== null && sn !== "" && sn !== undefined) {
             DictService.get({sn:sn},function(data){
+                if(data.state==="1"){
+                    $scope.selectstate = $scope.datas[1];
+//                    $scope.datas=["有效","无效"];
+                }else{
+                    $scope.selectstate = $scope.datas[0];
+//                    $scope.datas=["无效","有效"];
+                }
                 $scope.dict=data;
             });
             $scope.changeDict=function(){
+                $scope.dict.state=$scope.dict.selectstate.value;
                 $scope.dict.$promise = undefined;
                 $scope.dict.$resolved = undefined;
                 DictService.update({sn:sn}, $scope.dict, function () {
@@ -44,9 +50,10 @@ angular.module('Service.Dictnew', ['ngTable', 'ngResource'])
 
                     if($scope.dict.code===dictdates[i].code){
                         $scope.dict.name=dictdates[i].name;
+                        $scope.ngread=true;
                         break;
                     }
-                }
+                }};
              $scope.autoValidate=function(){
                for(var i=0;i<dictdates.length;i++){
                    if($scope.dict.code===dictdates[i].code){
@@ -56,19 +63,12 @@ angular.module('Service.Dictnew', ['ngTable', 'ngResource'])
                        }
                    }
                }
-             };
-              //  angular.forEach(dictdates,function(dictdata){
-                 //alert(code);
 
-                //    if($scope.dict.code===dictdata.code){
-                  //      $scope.dict.name=dictdata.name;
-
-                    //}
-               // });
             };
 
 
             $scope.changeDict=function(){
+                $scope.dict.state=$scope.selectstate.value;
                 $scope.dict.$promise = undefined;
                 $scope.dict.$resolved = undefined;
                 DictService.save($scope.dict,function(){
