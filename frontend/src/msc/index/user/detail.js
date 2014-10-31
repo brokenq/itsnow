7322066554881a6b133c9bb3eb2120affa5062fa
amrocky <amrocky@163.com>
2014-10-31 10:40:58 +0800

    create userdeail.js and deatil.jade

---
+++ frontend/src/msc/index/user/detail.js
@@ -0,0 +1,29 @@
+// List System
+angular.module('MscIndex.User.Detail', [ 'ngResource'])
+
+    .config(function ($stateProvider) {
+        $stateProvider.state('createUserDetail', {
+            url: '/createUserDetail',
+            templateUrl: 'user/detail.tpl.jade',
+            data: {pageTitle: '用户添加'}
+        });
+        $stateProvider.state('updateUserDetail',{
+            url:'/updateUserDetail/{id}',
+            templateUrl:'user/detail.tpl.jade',
+            data:{pageTitle:'用户修改'}
+        });
+    })
+    .controller('UserDetailCtrl', ['$rootScope','$scope', '$location', '$timeout', '$state','$stateParams', 'UserService',
+        function ($rootScope,$scope, $location, $timeout, $state,$stateParams, UserService) {
+            var id=$stateParams.id;
+            $scope.datas=[
+                {name: '启用',id:'1',state:'1'},
+                {name: '停用',id:'2',state:'2'}
+            ];
+            if (id !== null && id !== "" && id !== undefined) {
+
+            }else{
+
+            }
+        }
+    ]);
