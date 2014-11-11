ecf5e1b619600940c5b1f07499ff2cefc9f40484
brokenq <brokenq@163.com>
2014-11-11 13:08:03 +0800

    modify account sql for it

--- backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20140720142611_insert_accounts.sql
+++ backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20140720142611_insert_accounts.sql
@@ -7,7 +7,7 @@ VALUES
   (2, 'msu', 'Itsnow Enterprise User',  'msu', 'msu', 'Valid'),
   (3, 'msp', 'Itsnow Service Provider', 'msp', 'msp', 'Valid'),
   -- 由于注册功能有问题影响到集成测试，目前暂时先预先insert msp_test测试数据
-  (4, 'msp_test', 'Itsnow Service Provider', 'msp', 'msp', 'New')
+  (4, 'msp_test', 'Itsnow Service Provider Testor', 'msp_test', 'msp', 'New')
 ;
 
 
