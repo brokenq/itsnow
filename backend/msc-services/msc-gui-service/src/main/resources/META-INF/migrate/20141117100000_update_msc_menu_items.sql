6ec7365cddd30e2aeb84c62ad6220d8ffc40a783
Cao Jie <jacky_cao@dnt.com.cn>
2014-11-17 10:03:40 +0800

    add update msc menus sql of service-catalogs

---
+++ backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20141117100000_update_msc_menu_items.sql
@@ -0,0 +1,10 @@
+-- // update_msc_menu_items
+-- Migration SQL that makes the change goes here.
+
+UPDATE menu_items SET state = 'catalog.list'
+WHERE state = 'services.catalog';
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+
