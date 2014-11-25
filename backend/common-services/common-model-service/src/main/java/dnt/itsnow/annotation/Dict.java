7ae900a757a7169f75a38ff3df80a76d0371b136
Stone Xin <stone5751@126.com>
2014-11-25 15:27:53 +0800

    dictionary annotation

---
+++ backend/common-services/common-model-service/src/main/java/dnt/itsnow/annotation/Dict.java
@@ -0,0 +1,15 @@
+package dnt.itsnow.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <h1>数据字典注解</h1>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface Dict {
+    String code() default "";
+}
