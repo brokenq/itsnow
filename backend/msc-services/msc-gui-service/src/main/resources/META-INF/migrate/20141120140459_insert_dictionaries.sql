30a5bebee547b2f4b46721519ec04da4c34fd450
yuanming_pei <yuanming_pei@163.com>
2014-11-25 15:29:22 +0800

    change insert sql

--- backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20141120140459_insert_dictionaries.sql
+++ backend/msc-services/msc-gui-service/src/main/resources/META-INF/migrate/20141120140459_insert_dictionaries.sql
@@ -1,10 +1,10 @@
 -- // insert_dictionaries
 -- Migration SQL that makes the change goes here.
 
-INSERT INTO dictionaries(code, name, label, description, detail ) VALUES
-('001', '字典名称一', '北京', '描述一', '[{"key":"k11","value":"v11"},{"key":"k11","value":"v11"},{"key":"k11","value":"v11"}]'),
-('002', '字典名称二', '上海', '描述二', '[{"key":"k22","value":"v22"},{"key":"k22","value":"v22"},{"key":"k22","value":"v22"}]'),
-('003', '字典名称三', '广州', '描述三', '[{"key":"k33","value":"v33"},{"key":"k33","value":"v33"},{"key":"k33","value":"v33"}]');
+
+INSERT INTO dictionaries(code, name, label, detail) VALUES
+  ('001', 'area', '区域', '[{"key":"11","value":"北京"},{"key":"12","value":"天津"},{"key":"31","value":"上海"},{"key":"50","value":"重庆"}]');
+
 
 
 
