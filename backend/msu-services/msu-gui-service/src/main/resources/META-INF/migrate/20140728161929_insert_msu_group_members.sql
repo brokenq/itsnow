d01aca033cb0bca4e1496b96fda7377418276bc7
Stone <stone5751@126.com>
2014-11-28 14:18:10 +0800

    update insert_group_members

--- backend/msu-services/msu-gui-service/src/main/resources/META-INF/migrate/20140728161929_insert_msu_group_members.sql
+++ backend/msu-services/msu-gui-service/src/main/resources/META-INF/migrate/20140728161929_insert_msu_group_members.sql
@@ -1,12 +1,12 @@
 -- // insert_group_members
 -- Migration SQL that makes the change goes here.
 
-SET @administrator_name = 'admin';
-SET @admins_name = 'administrators';
-SET @administrators_gid = 1;
+SET @username = 'admin';
+SET @group_name = 'administrators';
+SET @group_id = (select id from groups where group_name = @group_name);
 
 INSERT INTO group_members(username, group_id, group_name)
-VALUES (@administrator_name, @administrators_gid, @admins_name);
+VALUES (@username, @group_id, @group_name);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
