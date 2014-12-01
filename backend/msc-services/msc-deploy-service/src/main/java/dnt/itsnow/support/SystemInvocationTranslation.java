6791d1f233df785ea31761fe346466461df16ad0
yuanming_pei <yuanming_pei@163.com>
2014-12-01 12:15:19 +0800

    change start_ms.sh and stop_ms.sh params to lower case

--- backend/msc-services/msc-deploy-service/src/main/java/dnt/itsnow/support/SystemInvocationTranslation.java
+++ backend/msc-services/msc-deploy-service/src/main/java/dnt/itsnow/support/SystemInvocationTranslation.java
@@ -195,7 +195,7 @@ public class SystemInvocationTranslation extends Bean implements SystemInvocatio
         return new RemoteInvocation(itsnowProcess.getHostAddress()) {
             @Override
             public int perform(Process process) throws Exception {
-                return process.run("./start_ms.sh", itsnowProcess.getName());
+                return process.run("./start_ms.sh", itsnowProcess.getName().toLowerCase());
             }
         };
     }
@@ -205,7 +205,7 @@ public class SystemInvocationTranslation extends Bean implements SystemInvocatio
         return new RemoteInvocation(itsnowProcess.getHostAddress()) {
             @Override
             public int perform(Process process) throws Exception {
-                return process.run("./stop_ms.sh", itsnowProcess.getName(), itsnowProcess.getProperty("http.port"));
+                return process.run("./stop_ms.sh", itsnowProcess.getName().toLowerCase(), itsnowProcess.getProperty("http.port"));
             }
         };
     }
